3ddb251389fe1b19a9fb674344d184fea45c6417
Uni-WLL <unipathwll@gmail.com>
2026-02-18 17:23:55 +0000

    your

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
+import json
+import os
+import google
+from google import genai
 
+
+client = genai.Client(api_key="")
+
+response = client.models.generate_content(
+    model="gemini-2.5-flash",
+    contents="Tell me a joke about robots"
+)
+
+print(response.txt)
